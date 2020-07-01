f89cd955935fa7014750cc717d952b18b20243de
cjscrimmy <craigscrimgeour@yahoo.com>
2020-07-01 17:55:24 +0100

    create root for getting distillery by id

--- src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -5,10 +5,7 @@ import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,6 +21,11 @@ public class DistilleryController {
             return new ResponseEntity<>(distilleryRepository.findDistilleryByRegion(region), HttpStatus.OK);
         }
         return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/distilleries/{id}")
+    public ResponseEntity getDistillery(@PathVariable Long id){
+        return new ResponseEntity<>(distilleryRepository.findById(id), HttpStatus.OK);
     }
 
 }
--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -47,4 +47,5 @@ public class WhiskyTrackerApplicationTests {
 		assertTrue(foundWhiskies.size() > 0);
 	}
 
+
 }
