7ece793b8ee88185563ced2b5f2d9b594a3f1feb
cjscrimmy <craigscrimgeour@yahoo.com>
2020-07-01 15:57:55 +0100

    create query for finding distilleries by region

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,6 +1,8 @@
 package com.codeclan.example.WhiskyTracker;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,6 +22,9 @@ public class WhiskyTrackerApplicationTests {
 	@Autowired
 	WhiskyRepository whiskyRepository;
 
+	@Autowired
+	DistilleryRepository distilleryRepository;
+
 	@Test
 	public void contextLoads() {
 	}
@@ -28,6 +33,12 @@ public class WhiskyTrackerApplicationTests {
 	public void findWhiskiesByYear(){
 		List<Whisky> foundWhiskies = whiskyRepository.findWhiskyByYear(1987);
 		assertTrue(foundWhiskies.size() > 0);
+	}
+
+	@Test
+	public void findDistilleriesByRegion(){
+		List<Distillery> foundDistilleries = distilleryRepository.findDistilleryByRegion("Highland");
+		assertTrue(foundDistilleries.size() > 0);
 	}
 
 }
